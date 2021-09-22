cbdfaa884da19c488a4d09817e04886204454858
Omar <42972288+OmarDroid@users.noreply.github.com>
2021-09-22 20:20:12 +0200

    Update native-lib.cpp

--- main/cpp/native-lib.cpp
+++ main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring
 
 JNICALL
 Java_com_android_kotlin_newsappmvvm_util_Keys_apiKey(JNIEnv *env, jobject object) {
-    std::string api_key = "a7c85cd6c0484925bb48b8b97f938eb3";
+    std::string api_key = "API_KEY";
     return env->NewStringUTF(api_key.c_str());
 }
